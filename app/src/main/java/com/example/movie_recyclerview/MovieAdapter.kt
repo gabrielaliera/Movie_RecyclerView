c82ae9005a68192f0471108cbb9dd1a1426f5f51
gabrielaliera <gliera2990@gmail.com>
2023-04-06 20:08:14 -0700

    Add onBindViewHolder code

--- app/src/main/java/com/example/movie_recyclerview/MovieAdapter.kt
+++ app/src/main/java/com/example/movie_recyclerview/MovieAdapter.kt
@@ -1,19 +1,27 @@
 package com.example.movie_recyclerview
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import org.json.JSONObject
 
-class MovieAdapter (private val movieList: List<String>): RecyclerView.Adapter<MovieAdapter.ViewHolder>( ){
+class MovieAdapter (private val movieList: List<JSONObject>): RecyclerView.Adapter<MovieAdapter.ViewHolder>( ){
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val movieImage: ImageView
+        val movieTitle: TextView
+        val movieRating: TextView
 
         init {
             // Find our RecyclerView item's ImageView for future use
             movieImage = view.findViewById(R.id.movie_image)
+            movieTitle = view.findViewById(R.id.movie_title)
+            movieRating = view.findViewById(R.id.movie_rating)
         }
     }
 
@@ -26,7 +34,32 @@ class MovieAdapter (private val movieList: List<String>): RecyclerView.Adapter<M
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Get element from your dataset at this position and replace the contents of the view with that element")
+       //Get element from your dataset at this position and replace the contents of the view with that element"
+
+        //Grab movie info
+        val movie = movieList[position]
+        Log.d("Movie 1st", "$movie")
+
+        //Get title and rating from json object
+        val title = movie.getString("title")
+        val voteAverage = movie.getString("vote_average")
+        Log.d("Movie Title/Average", "$title  $voteAverage")
+
+        //Set title and rating
+        holder.movieTitle.text = title
+        holder.movieRating.text = "Average Rating: $voteAverage"
+
+
+        //Get & Load Image
+        val image = "https://image.tmdb.org/t/p/w500/" + movie.getString("poster_path")
+        Log.d("Movie Image", "$image")
+
+        Glide.with(holder.itemView)
+            .load(image)
+            .centerCrop()
+            .into(holder.movieImage)
+
+        Log.d("Movie Image", "success")
     }
 
     override fun getItemCount() = movieList.size
