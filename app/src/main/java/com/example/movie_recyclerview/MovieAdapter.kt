e07c8d9dc49c8a70bbbf76757afaefb071a1b11e
gabrielaliera <gliera2990@gmail.com>
2023-04-06 20:58:22 -0700

    Add divider item decorator

--- app/src/main/java/com/example/movie_recyclerview/MovieAdapter.kt
+++ app/src/main/java/com/example/movie_recyclerview/MovieAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import org.json.JSONObject
@@ -36,6 +37,11 @@ class MovieAdapter (private val movieList: List<JSONObject>): RecyclerView.Adapt
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
        //Get element from your dataset at this position and replace the contents of the view with that element"
 
+        //Add Toast
+        holder.movieImage.setOnClickListener {
+            Toast.makeText(holder.itemView.context, "Movie at position $position clicked", Toast.LENGTH_SHORT).show()
+        }
+
         //Grab movie info
         val movie = movieList[position]
         Log.d("Movie 1st", "$movie")
@@ -51,7 +57,7 @@ class MovieAdapter (private val movieList: List<JSONObject>): RecyclerView.Adapt
 
 
         //Get & Load Image
-        val image = "https://image.tmdb.org/t/p/w500/" + movie.getString("poster_path")
+        val image = "https://image.tmdb.org/t/p/w500" + movie.getString("poster_path")
         Log.d("Movie Image", "$image")
 
         Glide.with(holder.itemView)
